6a6ec45aec2330e1f2ec2353421a7cd59b585e43
hama <75150231+hamanyann@users.noreply.github.com>
2024-06-06 16:38:42 +0900

    img

--- script.js
+++ script.js
@@ -8,34 +8,30 @@ const displayMessage = function (message) {
 
 const checkButton = document.querySelector('.check');
 const againButton = document.querySelector('.again');
-const guessInput = document.querySelector('.guess'); // guess input要素を取得
-const mainImage = document.getElementById('main-image'); // 画像要素を取得
-
+const guessInput = document.querySelector('.guess'); 
+const mainImage = document.getElementById('main-image'); 
+const subImage = document.getElementById('sub-image'); 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(guessInput.value);
   console.log(guess, typeof guess);
 
-  // When there is no input
   if (!guess) {
     displayMessage('数字を入れて？');
 
-    // When player wins
-  } else if (guess === secretNumber) {
+   } else if (guess === secretNumber) {
     displayMessage('正解です！');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#E1A1B9';
-    mainImage.src = 'img/img2.jpg';
+    mainImage.src = subImage.src;
 
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highScore').textContent = highScore;
     }
 
-    // Checkボタンを非表示にし、Againボタンを表示する
     checkButton.style.display = 'none';
     againButton.style.display = 'flex';
 
-    // When guess is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
       let guessEx = Math.abs(guess - secretNumber);
@@ -65,11 +61,10 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '1';
-
   document.querySelector('body').style.backgroundColor = '#fff';
 
   mainImage.src = 'img/img1.jpg';
-  // Againボタンを非表示にし、Checkボタンを表示する
+
   checkButton.style.display = 'block';
   againButton.style.display = 'none';
 });
